3503adce4a8c6ee49f855cf6099a6fafd62631a5
alexdecapri <alexdecapri@gmail.com>
2016-02-06 10:39:35 -0700

    one more prob

--- arrayReview.js
+++ arrayReview.js
@@ -17,7 +17,13 @@ function last(arr) {
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
 function rmvEvens(arr) {
-
+	var newArr = [];
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			newArr.push(arr[i]);
+		}
+	}
+	return newArr;
 }
 
 
@@ -37,18 +43,39 @@ var getRandomArbitrary = function() {
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+function randomNum(arr) {
+	var getRandomArbitrary = function() {
+  		return Math.floor(Math.random() * (30 - 0) + 0);
+	};
+	var random30 = getRandomArbitrary();
+	for (var i = 0; i < arr.length; i++) {
+		if (random30 === arr[i]) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
 
+// chose to return T/F instead of alert, because alert would pop up for each iteration
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
+//Create a copy of first and save it into second. Then, add 6 and 7 
+//to the end of second. When you run this, first should be just 
+//[1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created 
+//your copy correctly.
+
 var first = [1,2,3,4,5];
 var second;
 
   //code here
+
+  var second = first.slice(0); //creates copy
+  second.push(6, 7);
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
@@ -59,21 +86,51 @@ alert(second) //[1,2,3,4,5,6,7];
 
 
 
-//write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
+//write a function called longest that takes in our sentence 
+//variable, and returns the longest word in that sentence.
+
 var sentence = "Dev Mountain is the best"
 
-  //code here
+function longest(sentence) {
+	var words = sentence.split(" "); //this is an array
+	var longest = words[0];
+	for (var i = 0; i < words.length; i++) {
+		if (words[i].length > longest.length) {
+			longest = words[i];
+		}
+	}
+	/*  forEach method!! LEARN THIS
+	words.forEach(function(item) {
+		if (item.length > longest.length) {
+			longest = item;
+	});
+	*/
+	return longest;
+}
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//write a function called capitalize that takes in the myPoem variable and capitalizes every word 
-var myPoem = 'What is a jQuery but a misunderstood object?'
-//What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
+//write a function called capitalize that takes in the myPoem 
+//variable and capitalizes every word 
 
-  //code here
+var myPoem = 'What is a jQuery but a misunderstood object?'
+//What is a jQuery but a misunderstood object? --> What Is A JQuery 
+//But A Misunderstood Object?
+
+
+function capitalize(sentence) {
+	var words = sentence.split(" ");
+	var capsArr = [];
+
+	words.forEach(function(item) {
+		var capsWord = item
+		capsArr.push(item.substr(0, 1).toUpperCase() + item.substr(1)); //look into substring method if you forget how to do this
+	});
+	return capsArr.join(" "); //joins the array of words back together to one string of a sentence
+}
 
 
 
@@ -82,4 +139,25 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
 
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
-//Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+
+//Write a function called vowelCounter that takes in a string 
+//(theOdyssey) and returns how many vowels are in that string.
+
+function vowelCounter(str) {
+	var vowels = [];
+	var allLetters = str.split("");
+	for (var i = 0; i < allLetters.length; i++) {
+		if (allLetters[i] === "a" || "e" || "i" || "o" || "u") {
+			vowels.push(allLetters[i]);
+		}
+	}
+	return vowels.length;
+}
+
+
+
+
+
+
+
+
