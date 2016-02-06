e3dc8b955c2d8db93b2723ba08413a02c843704b
alexdecapri <alexdecapri@gmail.com>
2016-02-06 13:07:57 -0700

    half done

--- callbacksReview-two.js
+++ callbacksReview-two.js
@@ -35,14 +35,29 @@ var songs =
 
 
 //Above you have a large Array of Song Objects. 
-//Write a function that returns the songs array.  This function is acting like a getter.
+//Write a function that returns the songs array.  This function is acting 
+//like a getter.
 
-  //code here
+function returner() {
+    return songs;
+}
 
 
-//Now write a function that takes in two paramaters.  This function is acting like a setter.
-//The first parameter is a song object and the second parameter is a callback (which will be a placeholder for your getter function)
-//Inside your setter function first add the first parameter, or the song object, to the end of your songs array.
-//Once you do that, call your callback (getter) function which should get the songs and return them.
+//Now write a function that takes in two parameters.  This function is 
+//acting like a setter.
+//The first parameter is a song object and the second parameter is a 
+//callback (which will be a placeholder for your getter function)
+//Inside your setter function first add the first parameter, or the song 
+//object, to the end of your songs array.
+//Once you do that, call your callback (getter) function which should get 
+//the songs and return them.
   
-  //code here
+
+
+
+
+
+
+
+
+
