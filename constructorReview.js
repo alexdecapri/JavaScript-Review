87b16f5be6d2dc93272457a80b92996e8ca553ae
alexdecapri <alexdecapri@gmail.com>
2016-02-06 13:31:05 -0700

    about halfway through

--- constructorReview.js
+++ constructorReview.js
@@ -1,16 +1,34 @@
-//Create a Animal Constructor that has the following parameters. species, name, legs, color, food (which is an array of foods they can eat).
+//Create a Animal Constructor that has the following parameters. species, 
+//name, legs, color, food (which is an array of foods they can eat).
 
-  //code here
+function Animal(species, name, legs, color, food) {
+  this.species = species;
+  this.name = name;
+  this.legs = legs;
+  this.color = color;
+  this.food = food;
+}
 
 
-//Now create a person function that creates an object and returns it (not in constructor form) that has the following parameters. name, age, height, gender
+//Now create a person function that creates an object and returns it 
+//(not in constructor form) that has the following parameters. name, age, 
+//height, gender
 
-  //code here
+function person(name, age, height, gender) {
+  var personObj = {
+    name: name,
+    age: age,
+    height: height,
+    gender: gender
+  };
+  return personObj;
+}
 
 
 //Create a animal array and a person array.
 
-  //code here
+var animal1 = new Animal("giraffe", "bob", 4, "orange", "grass");
+var person1 = person("alex", 21, "6 feet", "male");
 
 
 //Create two instances of Animal and push those into your animal array
